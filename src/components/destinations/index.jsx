ab20ca95f53b4542bb28db09c5358503c3c1d619
karthikanairp4 <prasannakumarkarthikanair@gmail.com>
2024-10-02 11:10:23 -0500

    adding utils inside components for the sake of structure

--- src/components/destinations/index.jsx
+++ src/components/destinations/index.jsx
@@ -5,99 +5,11 @@ import image1 from '../../assets/reservation-images/bali.jpg';
 import image2 from '../../assets/reservation-images/taj.jpg';
 import image3 from '../../assets/reservation-images/dubai.jpg';
 import image4 from '../../assets/reservation-images/banff.jpg';
+import { destinations } from '../utils/dummy-data';
 
 const DestinationsScreen = () => {
   const navigate = useNavigate();
-  const tours = [
-    {
-      id: 1,
-      name: 'Destination 1',
-      price: '9999',
-      date: 'September 25th',
-      duration: '3-10 Days',
-      people: '10 People',
-      description:
-        'Destination number 1, here will be some description of location.',
-      url: image1,
-    },
-    {
-      id: 2,
-      name: 'Destination 2',
-      price: '9999',
-      date: 'September 25th',
-      duration: '3-10 Days',
-      people: '10 People',
-      description:
-        'Destination number 1, here will be some description of location.',
-      url: image2,
-    },
-    {
-      id: 3,
-      name: 'Destination 3',
-      price: '9999',
-      date: 'September 25th',
-      duration: '3-10 Days',
-      people: '10 People',
-      description:
-        'Destination number 1, here will be some description of location.',
-      url: image3,
-    },
-    {
-      id: 4,
-      name: 'Destination 4',
-      price: '9999',
-      date: 'September 25th',
-      duration: '3-10 Days',
-      people: '10 People',
-      description:
-        'Destination number 1, here will be some description of location.',
-      url: image4,
-    },
-    {
-      id: 5,
-      name: 'Destination 5',
-      price: '9999',
-      date: 'September 25th',
-      duration: '3-10 Days',
-      people: '10 People',
-      description:
-        'Destination number 1, here will be some description of location.',
-      url: image1,
-    },
-    {
-      id: 6,
-      name: 'Destination 6',
-      price: '9999',
-      date: 'September 25th',
-      duration: '3-10 Days',
-      people: '10 People',
-      description:
-        'Destination number 1, here will be some description of location.',
-      url: image2,
-    },
-    {
-      id: 7,
-      name: 'Destination 7',
-      price: '9999',
-      date: 'September 25th',
-      duration: '3-10 Days',
-      people: '10 People',
-      description:
-        'Destination number 1, here will be some description of location.',
-      url: image3,
-    },
-    {
-      id: 8,
-      name: 'Destination 8',
-      price: '9999',
-      date: 'September 25th',
-      duration: '3-10 Days',
-      people: '10 People',
-      description:
-        'Destination number 1, here will be some description of location.',
-      url: image4,
-    },
-  ];
+
 
   const handleClick = (id) => {
     navigate(`/destination-detail/${id}`);
@@ -108,7 +20,7 @@ const DestinationsScreen = () => {
       <div className={styles.tours}>
         <h1>Destinations</h1>
         <div className={styles.tourGrid}>
-          {tours.map((tour) => (
+          {destinations.map((tour) => (
             <div key={tour.id} className={styles.tourItem} onClick={() => handleClick(tour.id)}>
               <div className={styles.imagePlaceholder}>
                 <img src={tour.url} alt={tour.name} />
